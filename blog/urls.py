badcb19f9a44d81dbf42a632e9d4ecd683d22908
balian <balian@admin.com>
2024-12-07 17:31:47 +0200

    all

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),  # Головна сторінка з відображенням постів
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
 ]
